06b209acbcf40c0b975a2c5957c3905a0d90c4cd
saudqureshii <saudqureshi37@gmail.com>
2024-06-27 17:28:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,122 @@
+// navbar
+const navBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+navBtn.addEventListener("click", (e) => {
+  if (navLinks.classList.contains("open")) {
+    navLinks.classList.add("close");
+    navLinks.addEventListener(
+      "animationend",
+      (e) => {
+        navLinks.classList.remove("close");
+        navLinks.classList.remove("open");
+      },
+      { once: true }
+    );
+  } else {
+    navLinks.classList.add("open");
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.add("close");
+  navLinks.addEventListener(
+    "animationend",
+    (e) => {
+      navLinks.classList.remove("close");
+      navLinks.classList.remove("open");
+    },
+    { once: true }
+  );
+});
+
+//scroll bar down
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+scrollReveal().reveal("header__content a", {
+  ...scrollRevealOption,
+  delay: 1500,
+})
+
+
+//explore
+
+
+
+
+
+// FAQ
+  
+  
+const faq = document.querySelector(".faq__grid");
+
+faq.addEventListener("click", (e) => {
+  const target = e.target;
+  const faqCard = target.closest(".faq__card"); 
+  if (target.classList.contains("ri-arrow-down-s-line")) {
+    if (faqCard.classList.contains("active")) {
+      faqCard.classList.remove("active");
+    } else {
+      Array.from(faq.children).forEach((item) => {
+        item.classList.remove("active");
+      });
+      faqCard.classList.add("active");
+    }
+  }
+});
+
+//scroll
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("header").style.top = "0";
+    document.getElementById("scroll-up").style.display = "block";
+  } else {
+    document.getElementById("header").style.top = "-60px";
+    document.getElementById("scroll-up").style.display = "none";
+  }
+}
+
+//scroll-to-top
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById
+    ("progress");
+    let progressValue = document.getElementById
+    ("progress-value"); 
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100){
+      scrollProgress.style.display = "grid";
+    } else {
+      scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient
+    (#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  
